001338a6b3ff8000efa71c5581e4f1845fc3037a
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2017-12-19 15:25:07 +0530

    Create check whether you were born in leap year or not.c

---
+++ beginner/set1/check whether you were born in leap year or not.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+int year;
+scanf("%d",&year);
+if(year%4==0)
+{
+	if(year%100==0)
+	{
+		if(year%400==0)
+			printf("leap year");
+                        else 
+                                    printf("not a leap year");
+	}
+	else
+	printf("leap year");
+}
+else
+printf("not a leap year");
+	return 0;
+}
