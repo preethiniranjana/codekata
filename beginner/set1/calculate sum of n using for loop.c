64dbb9eb976640aaffd60e772a52d82c40a87a9e
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2017-12-19 15:36:07 +0530

    Create calculate sum of n using for loop.c

---
+++ beginner/set1/calculate sum of n using for loop.c
@@ -0,0 +1,13 @@
+    #include <stdio.h>
+    int main(void)
+    {
+    int i,sum=0,n;
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+    	sum=sum+i;
+    }
+    printf("%d",sum);
+    	return 0;
+    }
+     
