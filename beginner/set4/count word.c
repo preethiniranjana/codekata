a6742cda98275ca5b864bdaf9ce769fe2e341a8d
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2018-01-09 22:57:26 +0530

    Create count word.c

---
+++ beginner/set4/count word.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[100];
+    int len,count=0,i,w;
+    gets(s);
+    len=strlen(s);
+    for(i=0;i<len;i++)
+     {
+        if(s[i]==' ')
+        count++;   
+    }
+    w=count+1;
+    printf("%d",w);
+return 0;   
+}
