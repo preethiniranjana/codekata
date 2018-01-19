bd1990132003ae34830a1b3a6f183aa26e0f88e3
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2018-01-19 21:16:48 +0530

    Create print the string k times.c

---
+++ beginner/set5/print the string k times.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+char s[300];
+int k,i;
+scanf("%s",s);
+scanf("%d",&k);
+for(i=0;i<k;i++,printf("\n"))
+{
+	printf("%s",s);
+}
+	return 0;
+}
+ 
