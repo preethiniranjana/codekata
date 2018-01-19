e618f702dd92a5910f9ee8975cc86b4bce698dd5
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2018-01-19 16:02:24 +0530

    Create swap two numbers.c

---
+++ beginner/set4/swap two numbers.c
@@ -0,0 +1,13 @@
+    #include <stdio.h>
+     
+    int main(void)
+    {
+    	int x,y,temp;
+    	scanf("%d %d",&x,&y);
+    	temp=x;
+    	x=y;
+    	y=temp;
+    	printf("%d %d",x,y);
+    	return 0;
+    }
+     
