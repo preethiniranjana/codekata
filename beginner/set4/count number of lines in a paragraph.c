24681f2ed17c93dbe25a7a5118004d9dcafb62d7
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2018-01-19 15:33:03 +0530

    Create count number of lines in a paragraph.c

---
+++ beginner/set4/count number of lines in a paragraph.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[500];
+	int count=0,len,i;
+	scanf("%[^\n]s",s);
+	len=strlen(s);
+	for(i=0;i<=len;i++)
+	{
+		if(s[i]=='.')
+		count++;
+	}
+	printf("%d",count+1);
+	return 0;
+}
