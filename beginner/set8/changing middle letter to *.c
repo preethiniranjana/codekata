7432d04392d630d2997088359a376e4a430a1858
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2018-01-23 21:58:34 +0530

    Create changing middle letter to *.c

---
+++ beginner/set8/changing middle letter to *.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+char s[100];
+int n,d,i;
+scanf("%s",s);
+n=strlen(s);
+d=(n/2);
+if(n%2!=0)
+{
+	for(i=0;i<n;i++)
+	{
+		if(i==d)
+		{
+		printf("*");
+		}
+		else
+		{
+			printf("%c",s[i]);
+		}
+	}
+}
+if(n%2==0)
+{
+for(i=0;i<n;i++)
+	{
+		if(i==d-1)
+		{
+	            printf("*");
+		}
+		else if(i==d)
+            	{
+		printf("*");
+	            }
+		else
+		{
+			printf("%c",s[i]);
+		}
+	}	
+}
+
+
+
+	return 0;
+}
