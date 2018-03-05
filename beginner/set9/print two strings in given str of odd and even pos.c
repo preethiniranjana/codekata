c3cdd69977964883c981e2e77df6710fe7a6229b
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2018-03-05 21:28:09 +0530

    Create print two strings in given str of odd and even pos.c

---
+++ beginner/set9/print two strings in given str of odd and even pos.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char a[10];
+	int i,l;
+	scanf("%s",a);
+	l=strlen(a);
+	for(i=0;i<l;i++)
+	{
+		if(i%2==0)
+		{
+			printf("%c",a[i]);
+		}
+	}
+	printf(" ");
+	for(i=0;i<l;i++)
+	{
+		if(i%2==1)
+		{
+			printf("%c",a[i]);
+		}
+	}	
+	return 0;
+}
