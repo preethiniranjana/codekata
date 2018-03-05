0d4204b07739db7aa6572de66b6d31c63bdcca80
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2018-03-05 19:02:40 +0530

    Update difference.c

--- beginner/set9/difference.c
+++ beginner/set9/difference.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+ 
 int main(void)
 {
 int t,i,m,n;
@@ -6,6 +7,7 @@ scanf("%d",&t);
 for(i=0;i<t;i++)
 {
 	scanf("%d %d",&m,&n);
+	if((m<2^32)&&(n<2^32))
 	printf("%d\n",abs(m-n));
 }
 	return 0;
