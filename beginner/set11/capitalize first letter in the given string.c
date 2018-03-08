2df4ceae37080e0160e614a3733c679c6051d4e2
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2018-03-08 16:44:10 +0530

    Create capitalize first letter in the given string.c

---
+++ beginner/set11/capitalize first letter in the given string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char s[50];
+	int i;
+	scanf("%[^\n]s",s);
+	for(i=0;i<strlen(s);i++)
+	{
+		if(s[0]>='a'&&s[0]<='z')
+		{
+			s[0]-=32;
+		}
+	if(s[i]==' ')
+	{
+		s[i+1]-=32;
+	}
+	}
+	for(i=0;i<strlen(s);i++)
+	{
+		printf("%c",s[i]);
+	}
+	return 0;
+}
