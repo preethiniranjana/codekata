383b38b84e4dd757747490a63bf06d631c9298b6
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2018-03-08 22:47:31 +0530

    Update print kth odd num.c

--- beginner/set12/print kth odd num.c
+++ beginner/set12/print kth odd num.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 int main(void) 
 {
-	int i,n,k,array[50],count=1;
+	int i,n,k,a[50],count=1;
 	scanf("%d %d",&n,&k);
 	for(i=0;i<n;i++)
 	{
-		scanf("%d",&array[i]);
+		scanf("%d",&a[i]);
 	}
 	for(i=0;i<n;i++)
 	{
-		if(array[i]%2==1)
+		if(a[i]%2==1)
 		{
 			if(count==k)
 			{
-				printf("%d",array[i]);
+				printf("%d",a[i]);
 			}
 		count=count+1;
 		}
