42f8b8a941bd2340090819876b6df7706455d7cf
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2018-05-21 22:39:29 +0530

    Create even numbers in odd pos and vise versa.c

---
+++ hunter/set1/even numbers in odd pos and vise versa.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) 
+{
+int n,i,a[1000];
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d ",&a[i]);	
+}
+for(i=0;i<n;i++)
+{
+	if( i%2!=0 && a[i]%2==0)
+	{
+		printf("%d ",a[i]);
+	}
+	if(i%2==0 && a[i]%2!=0)
+	{
+		printf("%d ",a[i]);
+	}
+}
+	return 0;
+}
