3b5883e81f1a883a6f627f562c60c0a2ace045d0
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2018-06-15 16:43:58 +0530

    Create isomorphic.c

---
+++ player/set11/isomorphic.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+int a[1000],b[1000],x,y,i,j,flag=0;
+scanf("%s %s",a,b);
+int m=strlen(a);
+int n=strlen(b);
+if(m==n)
+{
+	for(i=0;i<m;i++)
+	{
+		for(j=i+1;j<m;j++)
+		{
+			x=a[i]-a[j];
+			y=b[i]-b[j];
+			if(x==y)
+			{
+				flag=1;
+			}
+			else
+			{
+				flag=0;
+				break;
+			}
+		}
+	}
+}
+if(flag==1)
+{
+	printf("YES");
+}
+else
+{
+	printf("NO");
+}
+	return 0;
+}
